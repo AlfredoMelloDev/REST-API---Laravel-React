ffd45f747b187d3f54511046ee679f800410087d
AlfredoMelloDev <alfredomellodeveloper@gmail.com>
2026-06-09 21:17:56 -0300

    Inicio do projeto APIrestful laravel + React

---
+++ eduteka-course-apirest-backend/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        // Iremos criar oito usuários
+        User::factory(8)->create([
+            // 'name' => 'Test User',
+            // 'email' => 'test@example.com',
+            'date_of_birth' => Carbon::now()->subYear(20)->format('Y-m-d') // Adicionamos a tabela date_of_birth
+        ]);
+    }
+}
